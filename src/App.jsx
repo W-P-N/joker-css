c695121c18b7f6af95e74e8d23253da74a8fbe2c
tanmayvaij <tanmayvaij22@gmail.com>
2022-12-18 12:37:03 +0530

    displayed all components

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,18 @@ import * as Buttons from "./Components/Button"
 const App = () => {
     return (
         <>
-        <Alerts.AlertPrimary>Hello! A Primary Warning</Alerts.AlertPrimary>
+
+        <Alerts.AlertPrimary>Hello</Alerts.AlertPrimary>
+        <Alerts.AlertDanger>Hello</Alerts.AlertDanger>
+        <Alerts.AlertSuccess>Hello</Alerts.AlertSuccess>
+        <Alerts.AlertWarning>Hello</Alerts.AlertWarning>
+
+        <Buttons.ButtonPrimary>Button</Buttons.ButtonPrimary>
+        <Buttons.ButtonWarning>Button</Buttons.ButtonWarning>
+        <Buttons.ButtonDanger>Button</Buttons.ButtonDanger>
+        <Buttons.ButtonSuccess>Button</Buttons.ButtonSuccess>
+        <Buttons.ButtonDark>Button</Buttons.ButtonDark>
+
         </>
     )
 }
