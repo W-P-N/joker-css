1b3a620f4a76ea09cbeac4a7f8fe9601f4d53c68
tanmayvaij <tanmayvaij22@gmail.com>
2022-12-18 13:07:34 +0530

    added react-router-dom package

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import * as Alerts from "./Components/Alert"
-import * as Buttons from "./Components/Button"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import * as Alerts from "./components/Alert"
+import * as Buttons from "./components/Button"
 
 const App = () => {
     return (
-        <>
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={ <Home/> }  />
+            </ Routes>
+            <Alerts.AlertPrimary>Hello</Alerts.AlertPrimary>
+            <Alerts.AlertDanger>Hello</Alerts.AlertDanger>
+            <Alerts.AlertSuccess>Hello</Alerts.AlertSuccess>
+            <Alerts.AlertWarning>Hello</Alerts.AlertWarning>
 
-        <Alerts.AlertPrimary>Hello</Alerts.AlertPrimary>
-        <Alerts.AlertDanger>Hello</Alerts.AlertDanger>
-        <Alerts.AlertSuccess>Hello</Alerts.AlertSuccess>
-        <Alerts.AlertWarning>Hello</Alerts.AlertWarning>
-
-        <Buttons.ButtonPrimary>Button</Buttons.ButtonPrimary>
-        <Buttons.ButtonWarning>Button</Buttons.ButtonWarning>
-        <Buttons.ButtonDanger>Button</Buttons.ButtonDanger>
-        <Buttons.ButtonSuccess>Button</Buttons.ButtonSuccess>
-        <Buttons.ButtonDark>Button</Buttons.ButtonDark>
-
-        </>
+            <Buttons.ButtonPrimary>Button</Buttons.ButtonPrimary>
+            <Buttons.ButtonWarning>Button</Buttons.ButtonWarning>
+            <Buttons.ButtonDanger>Button</Buttons.ButtonDanger>
+            <Buttons.ButtonSuccess>Button</Buttons.ButtonSuccess>
+            <Buttons.ButtonDark>Button</Buttons.ButtonDark>
+            
+        </BrowserRouter>
     )
 }
 
