394c2e35668331ed5991b78bee9784037fb30b8c
Parth <parthwani22@gmail.com>
2023-01-05 12:34:07 +0530

    Added card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,1 +1,14 @@
-export {}
+interface Children {
+    children: String | JSX.Element
+}
+
+export const Card = () => {
+    return (
+        <div className="border">
+            <img src="https://images.pexels.com/photos/6605310/pexels-photo-6605310.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="h-80" alt="" />
+            <h3>Hi there, I am Croissant</h3>
+            <p>Croissant sont tres bon!</p>
+            { }
+        </div>
+    )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import { ButtonPrimary } from "../components/Button";
+import { Card } from "../components/Card";
 
 export default function Home() {
     return (
         <div id="home">
             <ButtonPrimary>Primary button</ButtonPrimary>
+            <Card/>
         </div>
     )
 }
