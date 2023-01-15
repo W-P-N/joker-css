ca903d4ac0f8a275cc9119bd156dae2774399bb1
Parth <parthwani22@gmail.com>
2023-01-15 14:29:13 +0530

    Added card.

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,10 +4,13 @@ interface Children {
 
 export const Card = () => {
     return (
-        <div className="border">
-            <img src="https://images.pexels.com/photos/6605310/pexels-photo-6605310.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="h-80" alt="" />
-            <h3>Hi there, I am Croissant</h3>
-            <p>Croissant sont tres bon!</p>
+        <div className="border absolute shadow-md">
+            <img src="https://images.pexels.com/photos/6605310/pexels-photo-6605310.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="h-80 " alt="" />
+            <div className="text-center">
+                <h3>Hi there, I am Croissant</h3>
+                <hr />
+                <p>Croissant sont tres bon!</p>
+            </div>
             { }
         </div>
     )
--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,1 +1,17 @@
-export {}
+import { JSXElementConstructor } from "react"
+
+interface Children 
+{
+    children: String | JSX.Element
+}
+
+export const Carousel = () =>
+{
+    return 
+    (
+        <div className="">
+
+            { }
+        </div>
+    )
+}
